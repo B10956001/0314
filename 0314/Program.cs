69d8bd626e61edb9e230cf791fcee29cc2544247
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:03:51 +0800

    finish

--- 0314/Program.cs
+++ 0314/Program.cs
@@ -6,8 +6,28 @@ namespace _0314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
+            Console.Write("請輸入一個數字：");
+            string str = Console.ReadLine();
+            int str_num = Int32.Parse(str);
+            if (str_num % 2 != 0)
+            {
+                Console.WriteLine(str_num+"is weird");
+            }
+            else if (str_num % 2 == 0)
+            {
+                if (2 <= str_num && str_num <= 5)
+                {
+                    Console.WriteLine(str_num+"is not weird");
+                }
+                else if (6 <= str_num && str_num <= 20)
+                {
+                    Console.WriteLine(str_num+"is weird");
+                }
+                else
+                {
+                    Console.WriteLine(str_num+"is not weird");
+                }
+            }
         }
     }
 }
